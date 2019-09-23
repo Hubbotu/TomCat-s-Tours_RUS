1cf27ad01cfc6e610de465858c7e1898a8d8fc07
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2019-09-23 07:20:04 +0300

    Add files via upload

---
+++ ruRU.lua
@@ -0,0 +1,7 @@
+select(2, ...).TomCatsLibs.Locales.AddLocaleLookup("ruRU", {
+    ["Click to view Creature Details"] = "Нажмите, чтобы посмотреть информацию о существе",
+    ["Rare Creatures Log"] = "Журнал редких существ",
+    ["TUTORIAL_RaresLogNames"] = "Нажав на имя существа, вы получите более подробную информацию.",
+    ["TUTORIAL_RaresLogStars"] = "Нажав на звездочку, вы получите стрелку пути, если у вас установлен TomTom",
+    ["TUTORIAL_RaresLogToggle"] = "Отсюда можно переключать Журнал Заданий и Журнал Редких Существ.",
+})
